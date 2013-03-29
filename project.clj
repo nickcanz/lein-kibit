7698e434b86b1021deda6d29acb4659b5118706f
nickcanz <canzoneri.nicholas@gmail.com>
2013-03-29 00:16:14 -0400

    change kibit deps

--- project.clj
+++ project.clj
@@ -3,5 +3,5 @@
   :url "https://github.com/jonase/lein-kibit"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[jonase/kibit "0.0.8"]]
+  :dependencies [[jonase/kibit "0.0.9-SNAPSHOT"]]
   :eval-in-leiningen true)
